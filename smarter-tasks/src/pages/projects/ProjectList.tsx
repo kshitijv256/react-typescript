16d4d1cdac45276be8ab77b0bb96a7dc178d028e
Kshitij Verma <kshitijv250@gmail.com>
2023-07-23 22:55:57 +0530

    Nested projects route

--- smarter-tasks/src/pages/projects/ProjectList.tsx
+++ smarter-tasks/src/pages/projects/ProjectList.tsx
@@ -1,21 +1,9 @@
-import React, { useEffect } from "react";
-import { useProjectsDispatch } from "../../context/projects/context";
-import { fetchProjects } from "../../context/projects/actions";
+import React from "react";
 import ProjectListItems from "./ProjectListItems";
 
 const ProjectList: React.FC = () => {
-  // I'll define a new constant called dispatchProjects,
-  // to call the useProjectsDispatch() hook.
-  const dispatchProjects = useProjectsDispatch();
-
-  useEffect(() => {
-    // And I'll pass the `dispatchProjects` to `fetchProjects` function.
-    fetchProjects(dispatchProjects);
-  }, [dispatchProjects]);
   return (
     <div className="grid gap-4 grid-cols-4 mt-5">
-      {/*To keep this file clean, I'll move all the logic to access the projects 
-       from our app-state, to a new component ProjectListItems */}
       <ProjectListItems />
     </div>
   );
