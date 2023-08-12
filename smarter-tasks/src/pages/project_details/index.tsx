c2bea386e982dfde8c2ff854edd11036a98b2deb
Kshitij Verma <kshitijv250@gmail.com>
2023-08-12 20:19:16 +0530

    Made app production ready

--- smarter-tasks/src/pages/project_details/index.tsx
+++ smarter-tasks/src/pages/project_details/index.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-
-import ProjectDetails from "./ProjectDetails";
+import React, { Suspense } from "react";
+const ProjectDetails = React.lazy(() => import("./ProjectDetails"));
 import { TasksProvider } from "../../context/task/context";
+import ErrorBoundary from "../../components/ErrorBoundary";
 
 import { Outlet } from "react-router-dom";
 
 const ProjectDetailsIndex: React.FC = () => {
   return (
     <>
-      <TasksProvider>
-        <ProjectDetails />
-        <Outlet />
-      </TasksProvider>
+      <ErrorBoundary>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+          <TasksProvider>
+            <ProjectDetails />
+            <Outlet />
+          </TasksProvider>
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
